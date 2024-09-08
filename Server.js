20889d5bb97cbe0846ecf2a7e73f4873174dfe41
S4JITHvk <sajithvk04@gmail.com>
2024-09-08 15:46:29 +0530

    correct root path

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-require('./config/db');
+require('./config/Db');
 const app = express();
 const Router = require('./Routes/Router.js');
 const PORT=5000
