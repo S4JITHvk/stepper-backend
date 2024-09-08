501169161b8bdaeb1f3687afc419847dc0416f76
S4JITHvk <sajithvk04@gmail.com>
2024-09-08 15:48:58 +0530

    fixed bug

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-require('./config/Db');
+require('./config/Db.js');
 const app = express();
 const Router = require('./Routes/Router.js');
 const PORT=5000
